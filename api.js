cc3854cc3695c4f3352e52093963febb28176baf
Tyler Brigalli <59288324+Chronetic@users.noreply.github.com>
2021-03-19 11:18:15 -0400

    Update Register and Login API endpoints
    Login using a username 'login'.
    Register takes in more information now, including weight, age, calorie goal, etc.

--- api.js
+++ api.js
@@ -5,24 +5,25 @@ const jwt = require('./createJWT');
 
 exports.setApp = function (app, client)
 {
-    app.post('/api/register', async (req, res, next) =>
+app.post('/api/register', async (req, res, next) =>
     {
-        // Incoming: login, password, firstname, lastname, email (Maybe: Weight / Goal Weight)
+        // Incoming: login, email, password, firstName, lastName, calorieGoal, age, weight, goalWeight, height, gender 
         // Outgoing: id, error
         var error = '';
         var id = -1;  
-        const { email, password, firstName, lastName } = req.body;
+        const { login, email, password, firstName, lastName, calorieGoal, age, weight, goalWeight, height, gender } = req.body;
 
         const db = client.db();  
-        const results = await db.collection('Users').find({Email:email}).toArray(); 
-        if (results.length > 0)
+        const resultsE = await db.collection('Users').find({Email:email}).toArray();
+        const resultsU = await db.collection('Users').find({Login:login}).toArray();
+        if (resultsE.length > 0 || resultsU.length > 0)
         {
-            error = "User Already Exists";
+            error = "Username or Email Already Exists";
             id = -1;
         }
         else
         {       
-            const result = await db.collection('Users').insertOne({Email:email, Password:password, FirstName:firstName, LastName:lastName});
+            const result = await db.collection('Users').insertOne({Login:login, Email:email, Password:password, FirstName:firstName, LastName:lastName, CalorieGoal:calorieGoal, Age:age, Weight:weight, GoalWeight:goalWeight, Height:height, Gender:gender});
             id = result.insertedId;
         }
         var ret = { id:id, error:error };
@@ -33,12 +34,12 @@ exports.setApp = function (app, client)
     app.post('/api/login', async (req, res, next) => 
     {  
         // incoming: login, password  
-        // outgoing: id, firstName, lastName, error  
+        // outgoing: jwt access token or id of -1 to signify user doesn't exist
         var error = '';  
-        const { email, password } = req.body;  
+        const { login, password } = req.body;  
 
         const db = client.db();  
-        const results = await db.collection('Users').find({Email:email,Password:password}).toArray();
+        const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
 
         var id = -1;  
         var fn = '';  
@@ -53,9 +54,9 @@ exports.setApp = function (app, client)
             
             try 
             {
-                ret = jwt.createToken(fn, ln, id); 
+                ret = jwt.createToken(fn, ln, id);
             } 
-            catch (e) 
+            catch (e)
             {
                 ret = {error:e.message};
             }
