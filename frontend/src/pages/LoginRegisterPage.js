6356d9b27f9729c865002dbaee82650d9b21d56b
Chase Chambliss <chaseechambliss@gmail.com>
2021-03-26 00:43:12 -0400

    Login/Register Page Semi-Functional

--- frontend/src/pages/LoginRegisterPage.js
+++ frontend/src/pages/LoginRegisterPage.js
@@ -8,13 +8,13 @@ import background from '../media/background.mp4';
 const LoginRegisterPage = () =>
 {
     return(
-        <body>
-            <section class='container'>
+        <div id="body">
+            <section className='container'>
                 <LoginRegister />
             </section>
 
             <video src={background} loop autoPlay muted>Your browser does not support the video tag.</video>
-        </body>
+        </div>
     );
 };
 
