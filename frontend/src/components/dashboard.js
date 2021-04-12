e9cb16364d2586658158e6701759154a6d948340
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2021-04-12 16:08:45 -0400

    Dashboard.js stuff

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -2,19 +2,22 @@ import Navbar from "./navbar";
 import dataFetcher from "./dataFetcher";
 import DailyMealList from "./DailyMealList";
 import {BrowserRouter as Router} from "react-router-dom";
-
+import { useParams } from 'react-router-dom';
 
 const Dashboard = () =>
     {
-        const storage = require("../tokenStorage.js");
-        const token = storage.retrieveToken();
-        console.log(token);
-        console.log(token._id);
+        const {data} = useParams();
+        print(data);
+        // const storage = require("../tokenStorage.js");
+        // const token = storage.retrieveToken();
+        // console.log(token);
+        // console.log(token._id);
     const bp = require("./bp.js");
     //collect data for components
     //const { data: meals, isPending, error} = dataFetcher();
     //const { data: meals, isPending, error} = dataFetcher(bp.buildPath("api/mealtime"));
     const meals = null;
+
 
 
     return (
